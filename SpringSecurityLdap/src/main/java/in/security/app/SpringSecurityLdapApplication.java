559d788815b7a6e5ec2ff8bb9584ba22b58c7cf9
SushmithaB <sushmitha.buddha27@gmail.com>
2020-09-17 18:59:52 +0530

    Initial LDAP Authentication

---
+++ SpringSecurityLdap/src/main/java/in/security/app/SpringSecurityLdapApplication.java
@@ -0,0 +1,14 @@
+package in.security.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
+		
+	}
+
+}
---
+++ SpringSecurityLdap/src/test/java/in/security/app/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package in.security.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
